452295ec22310656c6308df18766ba432bb63bca
xaicron <xaicron@gmail.com>
2012-06-29 01:29:09 +0900

    initial commit

---
+++ lib/WWW/Google/Cloud/Messaging.pm
@@ -0,0 +1,42 @@
+package WWW::Google::Cloud::Messaging;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+WWW::Google::Cloud::Messaging -
+
+=head1 SYNOPSIS
+
+  use WWW::Google::Cloud::Messaging;
+
+=head1 DESCRIPTION
+
+WWW::Google::Cloud::Messaging is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2012 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
